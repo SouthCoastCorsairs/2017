aff584c1a8b652425e1eaf4e7d0f8ceb3e2d3c53
Justin Wolkowicz <justinw2200@gmail.com>
2017-03-19 16:07:39 -0400

    Update RopeClimber.java

--- Subsystems/RopeClimber.java
+++ Subsystems/RopeClimber.java
@@ -4,6 +4,7 @@ import org.usfirst.frc.team5846.robot.Robot;
 import org.usfirst.frc.team5846.robot.RobotMap;
 import org.usfirst.frc.team5846.robot.commands.RopeClimberCmd;
 
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -11,23 +12,27 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class RopeClimber extends Subsystem {
-	private Talon climbRope = new Talon(RobotMap.ROPE_CLIMBER);
+	public double climbSpeed = -0.3;
+	private Spark climbRope = new Spark(RobotMap.ROPE_CLIMBER_SPARK); //Climber motor Spark
+	private Talon climbRope2 = new Talon(RobotMap.ROPE_CLIMBER_TALON); //Climber motor Talon
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
     public void initDefaultCommand() {
-        setDefaultCommand(new RopeClimberCmd());
+        setDefaultCommand(new RopeClimberCmd()); //The default command of this subsystem
     	// Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
 
-	public void climbTheRope() {
-		while(Robot.oi.getButton1()){
-			climbRope.set(-1);
+	public void climbTheRope() {	//While button 1 is pressed, the rope climber motor is set to -0.3 speed
+		if (Robot.oi.getButton1()){			
+			climbRope.set(climbSpeed); //Speed of the climbing motor Spark
+			climbRope2.set(climbSpeed);//Speed of the climbing motor Talon
+		} else {
+			climbRope.set(0);
+			climbRope2.set(0);
 		}
-		climbRope.set(0);
-		
 		
 	}
 	
